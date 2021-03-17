9c88b7f32646e0021a6c42e827299193f4d940c5
marcostl2 <marcossilvatl2@gmail.com>
2021-03-16 23:22:17 -0400

    fix(little fix): n
    n

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -87,7 +87,7 @@
           href="https://github.com/vuejs/awesome-vue"
           target="_blank"
           rel="noopener"
-          >awesome-vue</a
+          >awesome-vue-js</a
         >
       </li>
     </ul>
